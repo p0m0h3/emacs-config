3686cc7d60036855a772e3fcda34417e40ad0b0e
Pooria Mokhtari <pooriamokhtarihesar@gmail.com>
2025-01-04 12:44:33 +0330

    Initial Commit

---
+++ init.el
@@ -0,0 +1,44 @@
+;; Pouria's Emacs Configuration!
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight regular :height 143 :width normal)))))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(monokai))
+ '(custom-safe-themes
+   '("95b0bc7b8687101335ebbf770828b641f2befdcf6d3c192243a251ce72ab1692" default))
+ '(package-selected-packages '(monokai-theme)))
+
+;; set theme
+(load-theme 'modus-vivendi t)
+
+;; don't show the startup message
+(setq inhibit-startup-message t)
+
+;; stop creating backup files
+(setq make-backup-files nil)
+
+;; disable auto saves
+(auto-save-mode -1)
+
+;; flash on bell
+(setq visible-bell t)
+
+;; disable tool-bar
+(tool-bar-mode -1)
+
+;; disable scroll-bar
+(scroll-bar-mode -1)
+
+;; display line numbers
+(global-display-line-numbers-mode 1)
+
+;; Enable transient mark mode
+(transient-mark-mode 1)
